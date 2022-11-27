4818c02239a0fa113407d88143292f9381a1825e
sudir <sudirdevlop@gmail.com>
2022-11-27 11:01:16 +0700

    install vue.js

--- public/js/app.js
+++ public/js/app.js
@@ -5327,12 +5327,10 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   mounted: function mounted() {
-    console.log('Component mounted.');
+    console.log("Component mounted.");
   }
 });
 
@@ -28165,15 +28163,9 @@ var staticRenderFns = [
       _c("div", { staticClass: "row justify-content-center" }, [
         _c("div", { staticClass: "col-md-8" }, [
           _c("div", { staticClass: "card" }, [
-            _c("div", { staticClass: "card-header" }, [
-              _vm._v("Example Component"),
-            ]),
+            _c("div", { staticClass: "card-header" }, [_vm._v("Contoh")]),
             _vm._v(" "),
-            _c("div", { staticClass: "card-body" }, [
-              _vm._v(
-                "\n                    I'm an example component.\n                "
-              ),
-            ]),
+            _c("div", { staticClass: "card-body" }, [_vm._v("Hallo dunia !!")]),
           ]),
         ]),
       ]),
